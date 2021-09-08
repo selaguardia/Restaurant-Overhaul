7a4b8e4644bed0112ca6c62c3834055f791d5b17
selaguardia <selaguardia91@gmail.com>
2021-09-08 11:12:55 -0700

    adds method-override dependency

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 const express = require("express");
 const app = express();
-const menuCtrls = require('./controllers/menu_controllers.js');
+const menuCtrls = require("./controllers/menu_controllers.js");
+const methodOverride = require("method-override");
 
 const PORT = 4000;
 
-
 // Middleware
-app.use((req, res, next) => {    
-	console.log(`${req.method} ${req.originalUrl}`);    
-	next();
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
 });
-app.use('/', menuCtrls);
+app.use("/", menuCtrls);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-
-
+app.use(methodOverride('_method'));
 
 // 404 page
 app.use("/*", (req, res) => {
   const context = { error: req.error };
   return res.status(404).render("404", context);
-})
+});
 
 app.listen(PORT, (req, res) => {
   console.log(`✅ Live on Port ${PORT} ✅`);
