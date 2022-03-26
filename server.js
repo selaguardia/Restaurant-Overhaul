992a0b4ff91bfd0fc6a8849e8c8a1c9262ed4386
selaguardia <selaguardia91@gmail.com>
2022-03-26 10:06:11 -0700

    fixes styling, restyles footer

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const MongoStore = require("connect-mongo");
 
 require("dotenv").config();
 
-const port = process.env.PORT || 3005; // might need to go above .config()
+const port = process.env.PORT || 3000; 
 
 app.use((req, res, next) => {
   console.log(`METHOD:${req.method} \nOG-URL:${req.originalUrl}`);
